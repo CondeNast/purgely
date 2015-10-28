ed7c47c41562cba479a980e0cad19365a5aa8e73
Zack Tollman <tollmanz@gmail.com>
2015-10-28 13:51:03 -0700

    Test config values are set.
    Test that the config values are set.

---
+++ test/tests/ConfigTest.php
@@ -0,0 +1,15 @@
+<?php
+
+class ConfigTest extends PHPUnit_Framework_TestCase {
+	public function test_constants_are_set() {
+		$this->assertTrue( defined( 'PURGELY_API_ENDPOINT' ) );
+		$this->assertTrue( defined( 'PURGELY_FASTLY_KEY' ) );
+		$this->assertTrue( defined( 'PURGELY_FASTLY_SERVICE_ID' ) );
+		$this->assertTrue( defined( 'PURGELY_ALLOW_PURGE_ALL' ) );
+		$this->assertTrue( defined( 'PURGELY_ENABLE_STALE_WHILE_REVALIDATE' ) );
+		$this->assertTrue( defined( 'PURGELY_STALE_WHILE_REVALIDATE_TTL' ) );
+		$this->assertTrue( defined( 'PURGELY_ENABLE_STALE_WHILE_ERROR' ) );
+		$this->assertTrue( defined( 'PURGELY_STALE_WHILE_ERROR_TTL' ) );
+		$this->assertTrue( defined( 'PURGELY_SURROGATE_CONTROL_TTL' ) );
+	}
+}
